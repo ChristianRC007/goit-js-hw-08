fc52c1f89a02cd3d459a8ff17c43b67007e177b2
Andrey Cherniak <baza904@gmail.com>
2021-03-02 22:17:20 +0200

    optimize code

--- js/task.js
+++ js/task.js
@@ -1,25 +1,27 @@
-import gallery from './gallery-items.js';
+import gallery from "./gallery-items.js";
 
 const refs = {
-  galleryList: document.querySelector('.js-gallery'),
-  lightboxRef: document.querySelector('.js-lightbox'),
-  lightboxImgRef: document.querySelector('.lightbox__image'),
+  galleryList: document.querySelector(".js-gallery"),
+  lightboxRef: document.querySelector(".js-lightbox"),
+  lightboxImgRef: document.querySelector(".lightbox__image"),
   lightboxBtn: document.querySelector('button[data-action="close-lightbox"]'),
-}
+};
 
-const originalImagesArray = gallery.map(el => el.original);
-const descriptionsArray = gallery.map(el => el.description);
+const originalImagesArray = gallery.map((el) => el.original);
+const descriptionsArray = gallery.map((el) => el.description);
 
-refs.galleryList.insertAdjacentHTML('afterbegin', galleryMarkup(gallery))
+refs.galleryList.insertAdjacentHTML("afterbegin", galleryMarkup(gallery));
 
-refs.galleryList.addEventListener('click', modalOpen);
-refs.lightboxRef.addEventListener('click', modalClose);
-refs.lightboxBtn.addEventListener('click', modalClose);
-window.addEventListener('keydown', onKeyPressEvents)
+refs.galleryList.addEventListener("click", modalOpen);
+refs.lightboxRef.addEventListener("click", modalClose);
+refs.lightboxBtn.addEventListener("click", modalClose);
+window.addEventListener("keydown", onKeyPressEvents);
 
 function galleryMarkup(data) {
   return data.reduce((acc, { original, preview, description }) => {
-    return acc + `<li class="gallery__item">
+    return (
+      acc +
+      `<li class="gallery__item">
   <a
     class="gallery__link"
     href="${original}"
@@ -32,64 +34,67 @@ function galleryMarkup(data) {
     />
     </a>
     </li>`
-  }, '')
-}
-  
+    )
+  }, "")
+};
+
 function modalOpen(e) {
-e.preventDefault();
-  if (e.target.localName === 'img') {
-    refs.lightboxRef.classList.add('is-open')
-    refs.lightboxImgRef.src = e.target.dataset.source
-    refs.lightboxImgRef.alt = e.target.alt
+  e.preventDefault();
+  if (e.target.localName === "img") {
+    refs.lightboxRef.classList.add("is-open");
+    refs.lightboxImgRef.src = e.target.dataset.source;
+    refs.lightboxImgRef.alt = e.target.alt;
   }
-}
+};
 
 function modalClose(e) {
-  if (e.target.localName !== 'img') {
-    refs.lightboxRef.classList.remove('is-open')
-    refs.lightboxImgRef.src = '/'
-    refs.lightboxImgRef.alt = ''
+  if (e.target.localName !== "img") {
+    toRemoveClassAndAttributes();
   }
-}
+};
 
-function onKeyPressModalClose(e) {
-  if (e.key === 'Escape') {
-    refs.lightboxRef.classList.remove('is-open')
-    refs.lightboxImgRef.src = '/'
-    refs.lightboxImgRef.alt = ''
-  }
+function onKeyPressModalClose() {
+  toRemoveClassAndAttributes();
 }
 
 function onKeyPressEvents(e) {
-  onKeyPressModalClose(e)
-  onRightKeyPress(e)
-  onLeftKeyPress(e)
+  if (e.key === "Escape") {
+    onKeyPressModalClose(e);
+  }
+
+  if (e.key === "ArrowRight") {
+    onRightKeyPress(e);
+  }
+
+  if (e.key === "ArrowLeft") {
+    onLeftKeyPress(e);
+  }
 }
 
-function onRightKeyPress(e) {
-    
-  if (e.key === 'ArrowRight') {
-      let currentImgIndex = originalImagesArray.indexOf(refs.lightboxImgRef.src);
-       
-      if (currentImgIndex + 1 > originalImagesArray.length - 1) {
-        currentImgIndex = -1
-      }
-     
-      refs.lightboxImgRef.src = originalImagesArray[currentImgIndex + 1]
-      refs.lightboxImgRef.alt = descriptionsArray[currentImgIndex + 1]
-    }
+function onRightKeyPress() {
+  let currentImgIndex = originalImagesArray.indexOf(refs.lightboxImgRef.src)
+
+  if (currentImgIndex + 1 > originalImagesArray.length - 1) {
+    currentImgIndex = -1;
+  }
+
+  refs.lightboxImgRef.src = originalImagesArray[currentImgIndex + 1];
+  refs.lightboxImgRef.alt = descriptionsArray[currentImgIndex + 1];
 }
 
-function onLeftKeyPress(e) {
-    
-  if (e.key === 'ArrowLeft') {
-      let currentImgIndex = originalImagesArray.indexOf(refs.lightboxImgRef.src);
-     
-    if (currentImgIndex - 1 < 0) {
-        currentImgIndex = originalImagesArray.length
-      }
-     
-      refs.lightboxImgRef.src = originalImagesArray[currentImgIndex - 1]
-      refs.lightboxImgRef.alt = descriptionsArray[currentImgIndex - 1]
-    }
+function onLeftKeyPress() {
+  let currentImgIndex = originalImagesArray.indexOf(refs.lightboxImgRef.src);
+
+  if (currentImgIndex - 1 < 0) {
+    currentImgIndex = originalImagesArray.length;
+  }
+
+  refs.lightboxImgRef.src = originalImagesArray[currentImgIndex - 1];
+  refs.lightboxImgRef.alt = descriptionsArray[currentImgIndex - 1];
+}
+
+function toRemoveClassAndAttributes() {
+  refs.lightboxRef.classList.remove("is-open");
+  refs.lightboxImgRef.src = "/";
+  refs.lightboxImgRef.alt = "";
 }
