4849e12c95948d06e0739b04ea7e0ffd9d47f9ec
Andrey Cherniak <baza904@gmail.com>
2021-03-02 22:50:04 +0200

    add formatter settings

--- js/task.js
+++ js/task.js
@@ -1,21 +1,21 @@
-import gallery from "./gallery-items.js";
+import gallery from './gallery-items.js';
 
 const refs = {
-  galleryList: document.querySelector(".js-gallery"),
-  lightboxRef: document.querySelector(".js-lightbox"),
-  lightboxImgRef: document.querySelector(".lightbox__image"),
+  galleryList: document.querySelector('.js-gallery'),
+  lightboxRef: document.querySelector('.js-lightbox'),
+  lightboxImgRef: document.querySelector('.lightbox__image'),
   lightboxBtn: document.querySelector('button[data-action="close-lightbox"]'),
 };
 
-const originalImagesArray = gallery.map((el) => el.original);
-const descriptionsArray = gallery.map((el) => el.description);
+const originalImagesArray = gallery.map(el => el.original);
+const descriptionsArray = gallery.map(el => el.description);
 
-refs.galleryList.insertAdjacentHTML("afterbegin", galleryMarkup(gallery));
+refs.galleryList.insertAdjacentHTML('afterbegin', galleryMarkup(gallery));
 
-refs.galleryList.addEventListener("click", modalOpen);
-refs.lightboxRef.addEventListener("click", modalClose);
-refs.lightboxBtn.addEventListener("click", modalClose);
-window.addEventListener("keydown", onKeyPressEvents);
+refs.galleryList.addEventListener('click', modalOpen);
+refs.lightboxRef.addEventListener('click', modalClose);
+refs.lightboxBtn.addEventListener('click', modalClose);
+window.addEventListener('keydown', onKeyPressEvents);
 
 function galleryMarkup(data) {
   return data.reduce((acc, { original, preview, description }) => {
@@ -34,45 +34,45 @@ function galleryMarkup(data) {
     />
     </a>
     </li>`
-    )
-  }, "")
-};
+    );
+  }, '');
+}
 
 function modalOpen(e) {
   e.preventDefault();
-  if (e.target.localName === "img") {
-    refs.lightboxRef.classList.add("is-open");
+  if (e.target.localName === 'img') {
+    refs.lightboxRef.classList.add('is-open');
     refs.lightboxImgRef.src = e.target.dataset.source;
     refs.lightboxImgRef.alt = e.target.alt;
   }
-};
+}
 
 function modalClose(e) {
-  if (e.target.localName !== "img") {
+  if (e.target.localName !== 'img') {
     toRemoveClassAndAttributes();
   }
-};
+}
 
 function onKeyPressModalClose() {
   toRemoveClassAndAttributes();
 }
 
 function onKeyPressEvents(e) {
-  if (e.key === "Escape") {
+  if (e.key === 'Escape') {
     onKeyPressModalClose(e);
   }
 
-  if (e.key === "ArrowRight") {
+  if (e.key === 'ArrowRight') {
     onRightKeyPress(e);
   }
 
-  if (e.key === "ArrowLeft") {
+  if (e.key === 'ArrowLeft') {
     onLeftKeyPress(e);
   }
 }
 
 function onRightKeyPress() {
-  let currentImgIndex = originalImagesArray.indexOf(refs.lightboxImgRef.src)
+  let currentImgIndex = originalImagesArray.indexOf(refs.lightboxImgRef.src);
 
   if (currentImgIndex + 1 > originalImagesArray.length - 1) {
     currentImgIndex = -1;
@@ -94,7 +94,10 @@ function onLeftKeyPress() {
 }
 
 function toRemoveClassAndAttributes() {
-  refs.lightboxRef.classList.remove("is-open");
-  refs.lightboxImgRef.src = "/";
-  refs.lightboxImgRef.alt = "";
+  refs.lightboxRef.classList.remove('is-open');
+  refs.lightboxImgRef.src = '/';
+  refs.lightboxImgRef.alt = '';
 }
+
+const fuck = 3;
+const fix = '32132';
